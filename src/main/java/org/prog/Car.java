2bbb21e53e986ea8c313d2f8dcd74d13cde223e0
Yuliya-Shaptala <arrow1503583yuliya@gmail.com>
2024-06-22 17:50:44 +0300

    Homework 1

--- src/main/java/org/prog/Car.java
+++ src/main/java/org/prog/Car.java
@@ -4,11 +4,15 @@ public class Car {
 
     public String color;
     public int milage = 0;
+    public int sum = 0;
 
     // TODO: add parameter that indicates distance car travels
-    public void goTo() {
+    public void goTo(int distance) {
         milage += 10;
+        sum +=distance;
+
         System.out.println(color + " car goes somewhere!");
         System.out.println(color + " car current milage is now " + milage);
+        System.out.println(color + " the car has traveled a distance - " + sum);
     }
 }
--- src/main/java/org/prog/Main.java
+++ src/main/java/org/prog/Main.java
@@ -2,33 +2,19 @@ package org.prog;
 
 public class Main {
 
-    /**
-     * #############################################
-     * #############################################
-     * #############################################
-     * ############[Car : car1 | color:Red]#########
-     * #############################################
-     * #############################################
-     * #############################################
-     * #############################################
-     * #############################################
-     * ###[Car : car2 | color:Blue]#################
-     * ############################10###############
-     *
-     * @param args
-     */
-
-
-    public static void main(String[] args) {
+       public static void main(String[] args) {
         Car car1 = new Car();
         Car car2 = new Car();
 
         car1.color = "Red";
         car2.color = "Blue";
 
-        car1.goTo();
+        car1.goTo(5);
         paintItBlack(car1);
-        car1.goTo();
+        car1.goTo(10);
+        car2.goTo(35);
+        car2.goTo(35);
+        car1.goTo(15);
     }
 
     public static void paintItBlack(Car carToPaint) {
